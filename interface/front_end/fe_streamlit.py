4ed17e691d7d01c4b5a10d3a2a797c6cb62749d0
Thomas Martin <trmb.martin@gmail.com>
2022-12-14 19:34:33 +0400

    Updates front-end: 14/12

--- interface/front_end/fe_streamlit.py
+++ interface/front_end/fe_streamlit.py
@@ -4,10 +4,9 @@ from PIL import Image
 import requests
 import re
 
-
 #Setting session_states
 if 'sidebar_state' not in st.session_state:
-    st.session_state.sidebar_state = 'collapsed'
+    st.session_state.sidebar_state = 'expanded'
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -20,31 +19,59 @@ if 'history' not in st.session_state:
 
 #Header
 st.set_page_config(
-    page_title = 'Dr. Genius',
-    page_icon=":heart:",
+    page_title = 'Charly',
+    page_icon=":sun_behind_cloud:",
     initial_sidebar_state=st.session_state.sidebar_state)
 
-#Sidebar and Refresh
-f1, f2 = st.columns([1, 0.16])
+#Sidebar and Refresh buttons style
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    height: 2em;
+    width: 5em;
+    border-radius:5px;
+    border:1px solid #000000;
+    font-size:25px;
+    font-weight: bold;
+    display: block;
+}
+
+div.stButton > button:hover {
+ background-color: #ECF5ED;
+ color: black;
+ border:2px solid #000000;
+ font-weight: bold;
+}
+
+div.stButton > button:active {
+	position:relative;
+    border:2px solid #000000;
+	top:2px;
+}
+
+</style>""", unsafe_allow_html=True)
+
+
+f1, f2 = st.columns([3,0.7])
 with f1:
     #Side bar button
-    if st.button('Click for more resources'):
+    if st.button('Close sidebar'):
         st.session_state.sidebar_state = 'expanded' if st.session_state.sidebar_state == 'collapsed' else 'collapsed'
         # Force an app rerun after switching the sidebar state.
         st.experimental_rerun()
 with f2:
-    if st.button('Refresh'):
+    if st.button('Clear chat'):
         st.session_state['generated'] = []
         st.session_state['past'] = []
         st.session_state['history'] = []
 
-st.title('Welcome to Dr.Genius')
+#Welcome message / title
+st.title("Hi, I'm Charly! ")
 
 
 #API
-# api_token = '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'
-API_URL = "https://mhconvoai-kk3ee7qoyq-ew.a.run.app/predict"
-# headers = {"Authorization": f"Bearer {api_token}"}
+API_URL = "https://mhconvai-full-kk3ee7qoyq-ew.a.run.app/predict"
+
 
 #API Functions
 def query(params):
@@ -56,7 +83,7 @@ def get_text():
     return input_text
 
 # Chat input from User
-user_input = st.text_input("What would you like to talk about?")
+user_input = st.text_input("What would you like to chat about?")
 
 #Set history for query
 history = st.session_state.history
@@ -82,34 +109,29 @@ if st.session_state['generated']:
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
         message(output_message, key=str(i))
 
-# st.write(st.session_state.past)
-# st.write(st.session_state.generated)
 
 
-#Sidebar section
-image = "https://images.everydayhealth.com/images/how-to-find-a-therapist-whos-right-for-you-1440x810.jpg?w=1110"
+#####################
+####### Sidebar content
+#####################
+
+image = 'https://i.postimg.cc/cLbyPzKz/logo-chat-1.png'
+# In case our logo hosting is KO, use "https://images.everydayhealth.com/images/how-to-find-a-therapist-whos-right-for-you-1440x810.jpg?w=1110"
 
 
 with st.sidebar:
 
-    #st.title("Welcome to Dr. Genius")
+    st.image(image,width=100,use_column_width='auto')
 
-    st.image(image, caption='Fancy some therapy?')
 
+
+    #####################
+    ####### Expanders
+    #####################
     st.markdown("""
-    -----------------------------------
-    This chatbot was created by Le Wagon Students.
-    It aims at acting as a *'virtual therapist'* anyone can talk to.
-    To use the chatbot:
-    - Type a sentence of your choice
-    - Wait for Dr. Genius' answer
-    - That's it!
-    To learn more, visit our [Github Page]("https://github.com/sophbla/MHConvoAI").
-""")
-
-    "&nbsp;"
-
-    expander = st.expander("Crisis hotlines Info.")
+    Additional resources :point_down:
+    """)
+    expander = st.expander("Crisis hotlines Info")
     expander.write(""" These hotlines are for anyone who's struggling. They won't judge you. Free, anonymous, and always open.
     - **Samaritans** | Phone: 116 123 - [https://www.samaritans.org](https://www.samaritans.org)
     - **SHOUT** | Text: 85258 - [https://giveusashout.org](https://giveusashout.org)
@@ -123,10 +145,37 @@ with st.sidebar:
 
     """)
 
-    expander = st.expander("Disclaimer")
-    expander.write("""
-    *All content and information on this website if for informational and educational purposes only and does NOT constitute medical advice*.
-""")
+    #####################
+    ####### Text info
+    #####################
+
+    st.markdown("""
+    -----------------------------------
+    This chatbot was created by Le Wagon Students.
+    It's a *virtual companion* anyone can talk to.
+    To chat with Charly:
+    - Type a sentence of your choice
+    - Wait for Charly's answer
+    - That's it!
+    To learn more, visit our [Github Page]("https://github.com/sophbla/MHConvoAI").
+
+
+
+    """)
+
+    #####################
+    ####### Disclaimer style & content
+    #####################
+    st.markdown(""" <style> .disclaimer {
+    font-size: 10px; color: #999999;font-style: italic}
+    </style> """, unsafe_allow_html=True)
+
+    st.markdown("""<p class='disclaimer'>
+    Disclaimer: All content and information on this website if for informational and educational purposes only and does NOT constitute medical advice.
+    </p>""", unsafe_allow_html=True)
+
+
+
 
 
 #st.write(st.session_state["generated"])
